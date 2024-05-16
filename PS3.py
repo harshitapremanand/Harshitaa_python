bce207bfebd046b28e2e30a1cb2a2cfc044ef2b9
harshitapremanand <169056038+harshitapremanand@users.noreply.github.com>
2024-05-16 18:31:23 +0530

    Update PS3.py
    Added few more codes

--- PS3.py
+++ PS3.py
@@ -11,3 +11,22 @@ marks = [58, 99, 76, 85]
 
 lang_mark = dict(zip(names, marks))
 print(lang_mark)
+
+actual_marks = []
+actual_marks.append(eng_mark)
+actual_marks.append(lang_mark)
+print(actual_marks)
+
+totalMarks = []
+for marks in actual_marks:
+    total_marks_dict ={}
+    for keys in marks:
+        total_marks_dict[keys] = eng_mark[keys] + lang_mark[keys]
+totalMarks.append(total_marks_dict)
+print(totalMarks)
+
+class_marks = {}
+list_of_names_sub_tot = [("Harsh","Eng", "Lang", "Tot"), ("Aadi", "Eng", "Lang", "Tot"), ("Karunesh", "Eng", "Lang", "Tot"), ("Sam", "Eng", "Lang", "Tot") ]
+for i in list_of_names_sub_tot:
+    class_marks.update({i : None})
+print(class_marks)
